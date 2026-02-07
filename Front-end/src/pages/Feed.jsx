963874247483166b89495002787244197247460c
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-07 13:18:12 +0530

    Update Feed.jsx

--- Front-end/src/pages/Feed.jsx
+++ Front-end/src/pages/Feed.jsx
@@ -6,9 +6,7 @@ const Feed = () => {
 
     const [ posts, setPosts ] = useState([
         {
-            _id: "1",
-            image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-            caption: "Beautiful scenery",
+           
         }
     ])
 
@@ -54,3 +52,4 @@ const Feed = () => {
 
 
 export default Feed
+

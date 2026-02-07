0074dc51ef6edb723763e9232a0a2636f10d0d13
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-07 19:48:58 +0530

    Update Feed.jsx

--- Front-end/src/pages/Feed.jsx
+++ Front-end/src/pages/Feed.jsx
@@ -3,16 +3,17 @@ import axios from "axios"
 import { Link } from 'react-router-dom'
 alert("Wait for 5-10 seconds & kindly refresh your page if your new post is not visible.")
 const Feed = () => {
-
     const [ posts, setPosts ] = useState([
         {
-           
+            // _id: "1",
+            // image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+            // caption: "Beautiful scenery",
         }
     ])
-
+    
     useEffect(()=>{
 
-        axios.get("https://post-interest-com.onrender.com/posts")
+        axios.get("http://localhost:3000/posts")
         .then((res)=>{
 
             setPosts(res.data.posts)
@@ -38,6 +39,9 @@ const Feed = () => {
                         <div key={post._id} className='post-card' >
                             <img src={post.image} alt={post.caption} />
                             <p>{post.caption}</p>
+                            <p>
+                                Posted at: {new Date(post.createdAt).toLocaleString()}
+                            </p>
                         </div>
                     ))
                 ) : (
@@ -50,7 +54,4 @@ const Feed = () => {
     )
 }
 
-
 export default Feed
-
-

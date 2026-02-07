1318ec9676b7449084a68c13dfcf9f0736f59ac0
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-07 19:47:45 +0530

    Update post.model.js

--- Back-end/src/models/post.model.js
+++ Back-end/src/models/post.model.js
@@ -2,6 +2,8 @@ const mongoose=require('mongoose')
 const postSchema=new mongoose.Schema({
     image:String,
     caption:String,
+},{
+    timestamps:true   // adds createdAt & updatedAt
 })
 const postModel=mongoose.model("allPosts",postSchema)
 

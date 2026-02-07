ddcdacd104e84bfe5ec658dd85573f5c708f4b66
Rishabh Dubey <rishabhdubey192005@gmail.com>
2026-02-07 11:38:53 +0530

    Update Feed.jsx

--- Front-end/src/pages/Feed.jsx
+++ Front-end/src/pages/Feed.jsx
@@ -14,7 +14,7 @@ const Feed = () => {
 
     useEffect(()=>{
 
-        axios.get("http://localhost:3000/posts")
+        axios.get("https://post-interest-com.onrender.com/posts")
         .then((res)=>{
 
             setPosts(res.data.posts)
@@ -51,5 +51,6 @@ const Feed = () => {
 
     )
 }
+
 
 export default Feed
